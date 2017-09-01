0fbff995fbba92416e66470ca8ae1a9ebaeac9a5
Charu <Chhimpacharu@gmail.com>
2017-09-01 14:31:44 +0530

    Update media.py

--- media.py
+++ media.py
@@ -1,12 +1,12 @@
 import webbrowser
 
 class Movie():
-	""" It is a class which will give you all the information about a movie """
+	""" init function to initialize the variablesprovide memory space to the new objects """
 	def __init__(self, title, storyline, poster_image_url, trailer_youtube_url):
 		self.title = title
 		self.storyline = storyline
 		self.poster_image_url = poster_image_url
 		self.trailer_youtube_url = trailer_youtube_url
-
+	""" show_trailer function to open a browser window and show the trailer"
 	def show_trailer(self):
 		webbrowser.open(self.trailer_youtube_url)
