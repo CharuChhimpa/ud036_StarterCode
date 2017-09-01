c10d11cb649130654d9cd5c73b6d2b209e4025d1
Charu <Chhimpacharu@gmail.com>
2017-09-01 12:53:07 +0530

    Update entertainment.py

--- entertainment.py
+++ entertainment.py
@@ -1,6 +1,8 @@
 import media
 import fresh_tomatoes
 
+# Making several instances of the class Movie
+
 toy_story = media.Movie("Toy story", "A boy and toys", "images/toy_story.jpg", "https://www.youtube.com/watch?v=KYz2wyBy3kc")
 
 avatar = media.Movie("Avatar", "Animated Creatures", "images/avatar.jpg", "https://www.youtube.com/watch?v=d1_JBMrrYw8")
@@ -14,6 +16,8 @@ inception = media.Movie("Inception", "Inception" ,"images/inception.jpg", "https
 hunger_games = media.Movie("Hunger Games", "Advanterous movie with thrill" ,"images/hunger_games.jpg", "https://www.youtube.com/watch?v=mfmrPu43DF8")
 
 movies = [toy_story, avatar, annabelle, grudge, inception, hunger_games]
+
+# Calling fresh_tomatoes function open_movies_page to make a website
 
 fresh_tomatoes.open_movies_page(movies)
 #print(movie_website.Movie.__doc__)
