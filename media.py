c153f2d7300ac91fcc300635f98ad68a60ec1967
Charu <Chhimpacharu@gmail.com>
2017-09-02 08:24:50 +0530

    Update media.py

--- media.py
+++ media.py
@@ -3,11 +3,13 @@ import webbrowser
 
 class Movie():
     """init function to initialize the variables and provide memory space"""
-    def __init__(self, title, storyline, poster_image_url, trailer_youtube_url):
+    def __init__(self, title,
+                 storyline, poster_image_url, trailer_youtube_url):
         self.title = title
         self.storyline = storyline
         self.poster_image_url = poster_image_url
         self.trailer_youtube_url = trailer_youtube_url
+
     """show_trailer function to open a browser window and show the trailer"""
     def show_trailer(self):
-        webbrowser.open(self.trailer_youtube_url)  
+        webbrowser.open(self.trailer_youtube_url)
